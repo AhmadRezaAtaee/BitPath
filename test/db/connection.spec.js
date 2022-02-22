fe042fc66d2bae4b87f826489b2046d02286c55d
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-22 23:18:59 +0330

    refactor: add ping test

--- test/db/connection.spec.js
+++ test/db/connection.spec.js
@@ -5,19 +5,24 @@ const testKey = 'test'
 const testVal = 'redis: ok.'
 
 describe('Redis Connection', () => {
-	before(async () => {
+	before('Ping', async () => {
+		const res = await redis.PING()
+		expect(res).to.string('PONG')
+	});
+
+	it('Set Value', async () => {
 		const res = await redis.set(testKey, testVal)
-		expect(res).to.equal('OK')
+		expect(res).to.string('OK')
 	});
 
 	it('Get Value', async () => {
 		const res = await redis.get(testKey)
-		expect(res).to.equal(testVal)
+		expect(res).to.string(testVal)
 	});
 
 	after(async () => {
 		const res = await redis.del(testKey)
-		expect(res).to.equal(1)
+		expect(res).equal(1)
 	});
-	
+
 });
