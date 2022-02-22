383bdf2673a50b763a4da78b43ac2e876a6665bf
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-22 23:19:38 +0330

    feat: add insert method test

--- test/db/repository.spec.js
+++ test/db/repository.spec.js
@@ -1,9 +1,29 @@
 const Urls = require('../../src/repository/index');
+const db = require('../../src/repository/connection');
 const { expect } = require('chai');
+const ShortLink = require('../../src/repository/short-link');
 
-describe('Redis Connection', () => {
-	it('Insert Url', () => Urls.insert);
-	it('Get Url', () => Urls.get);
-	it('Get All Urls', () => Urls.all);
-	it('Update Url Views', () => Urls.updateViews);
+const fullUrl = 'https://redis.io/commands'
+const genCode = ShortLink.genCode
+const codes = ['1co2de3', '1co2de3', '1te2st3']
+let uniqueCount = 0
+ShortLink.genCode = async () => {
+	return codes[uniqueCount++]
+}
+
+describe('Repository', () => {
+	it('Insert Url (unique check)', async () => {
+		let url = await Urls.insert(fullUrl)
+		expect(url.code).string(codes[0])
+		url = await Urls.insert(fullUrl)
+		expect(url.code).string(codes[2])
+		expect(uniqueCount).equal(3)
+	});
+
+	it('Insert Url', async () => {
+		const full = fullUrl
+		ShortLink.genCode = genCode
+		const url = await Urls.insert(full)
+		expect(url.fullUrl).equal(full)
+	});
 });
