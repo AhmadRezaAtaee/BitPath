fa969955fa5fe33a7a7bcb23db0c3751ac419c8c
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-23 23:21:55 +0330

    feat: add get all method test

--- test/db/repository.spec.js
+++ test/db/repository.spec.js
@@ -1,5 +1,6 @@
 const Urls = require('../../src/repository/index');
 const db = require('../../src/repository/connection');
+const indexes = require('../../src/repository/db-index');
 const { expect } = require('chai');
 const ShortLink = require('../../src/repository/short-link');
 
@@ -10,6 +11,10 @@ let uniqueCount = 0
 ShortLink.genCode = async () => {
 	return codes[uniqueCount++]
 }
+const fullUrls = [
+	'pc.io', 'node.net', 'google.com',
+	'cats.shop', 'test.com', 'js.co',
+]
 
 describe('Repository', () => {
 	it('Insert Url (unique check)', async () => {
@@ -33,12 +38,38 @@ describe('Repository', () => {
 		expect((await Urls.get(url.code)).code).equal(url.code)
 	});
 
+	it('Get All Urls', async () => {
+		ShortLink.genCode = genCode
+		expect(await indexes.create()).string('OK')
+		const codes = []
+		for (let index = 0; index < fullUrls.length; index++) {
+			const element = fullUrls[index];
+			const shortLink = await Urls.insert(element)
+			expect(shortLink.code).exist
+			codes.push(shortLink.code)
+		}
+		expect(codes.length).equal(fullUrls.length)
+
+		let find = await Urls.all({ search: 'co' })
+		expect(find.total).equal(3)
+
+		find = await Urls.all({ limit: 2, part: 0 })
+		expect(find.documents.length).equal(2)
+		expect(
+			[find.documents[0].value.code, find.documents[1].value.code]
+		).deep.equal([codes[0], codes[1]])
+
+		find = await Urls.all({ sort: 'desc' })
+		expect(find.documents.map(doc => doc.id)).deep.equal(codes.reverse())
+	});
+
 	it('Update Url Views', async () => {
 		const url = await Urls.insert(fullUrl)
 		expect(new Array(await Urls.updateViews(url.code)).flat()[0]).equal(1)
 	});
 
-	after('Delete all keys', async () => {
-		expect(await db.FLUSHALL()).string('OK')
-	})
+	afterEach('Delete all keys', async () => {
+		const clean = await db.FLUSHDB()
+		expect(clean).equal('OK')
+	});
 });
