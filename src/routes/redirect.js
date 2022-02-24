38a354364ed818521d33dbb44b8b8824976077a7
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-25 00:25:42 +0330

    fix: redirect path key

--- src/routes/redirect.js
+++ src/routes/redirect.js
@@ -3,11 +3,11 @@ const { join } = require('path')
 const { Urls } = require('../repository');
 
 router
-    .get(join(process.env.REDIRECT_PATH || '/', '/:shortUrl'), (req, res) => {
+    .get(join(process.env.SHORTENER_REDIRECT_PATH || '/', '/:shortUrl'), async (req, res) => {
         try {
             /* Get the full link from the database according to the
             short link received in the redirect route parameters */
-            let url = Urls.get(req.params.shortUrl);
+            let url = await Urls.get(req.params.shortUrl);
             if (!url) {
                 res.status(404).send({ error: 'not found!' });
                 return;
@@ -15,8 +15,8 @@ router
 
             /* Redirect the user to the full url if any. After being redirected,
             the number of visits to the shortened link in the database will increase */
-            res.redirect(/^(https?:\/\/)/.test(url.full) ? url.full : "http://" + url.full);
-            Urls.updateViews(url.id);
+            res.redirect(/^(https?:\/\/)/.test(url.fullUrl) ? url.fullUrl : "http://" + url.fullUrl);
+            Urls.updateViews(url.code);
 
         } catch (error) {
             res.status(500).send({ error: "something went wrong!" });
