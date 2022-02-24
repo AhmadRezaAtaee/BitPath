8211c3349d41a8d3f9e908dfa865d072bbbc6ea6
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-24 11:59:30 +0330

    feat: add middlewares

---
+++ src/middlewares/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+module.exports = [
+    // to access the body of http requests
+    express.json(),
+    
+    /*
+    check the json in the body of the request. To avoid the problem of displaying errors and project information on the user side.
+    when a json invalid is sent, all the contents of the file and the information related to the project are displayed with an error.
+    */
+   (err, req, res, next) => {
+       if (['POST', 'PUT'].includes(req.method) && err) {
+           return res.status(400).send({ error: "invalid json in the body of the request! please check the structure and syntax of json." });
+        }
+        next()
+    },
+
+    // to access the form url encoded http requests
+    express.urlencoded({ extended: true }),
+
+    // serve static files exist in public folder
+    express.static(`./public`),
+]
