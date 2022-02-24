44c907b85bd8bc4a9b55b1ae087fbb2088e68267
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-24 12:16:10 +0330

    feat: add get code and redirect end point

--- src/routes/redirect.js
+++ src/routes/redirect.js
@@ -1,4 +1,27 @@
 const router = require('express').Router();
+const { join } = require('path')
 const Urls = require('../database/urls-repository');
+
+router
+    .get(join(process.env.REDIRECT_PATH || '/', '/:shortUrl'), (req, res) => {
+        try {
+            /* Get the full link from the database according to the
+            short link received in the redirect route parameters */
+            let url = Urls.get(req.params.shortUrl);
+            if (!url) {
+                res.status(404).send({ error: 'not found!' });
+                return;
+            }
+
+            /* Redirect the user to the full url if any. After being redirected,
+            the number of visits to the shortened link in the database will increase */
+            res.redirect(/^(https?:\/\/)/.test(url.full) ? url.full : "http://" + url.full);
+            Urls.updateViews(url.id);
+
+        } catch (error) {
+            res.status(500).send({ error: "something went wrong!" });
+            console.log(error.message)
+        }
+    })
 
 module.exports = router;
