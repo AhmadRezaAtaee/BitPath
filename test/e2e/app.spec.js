7bdabd13d67237a68dec43dc515829f3709254de
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-25 00:59:33 +0330

    feat: add create short url test

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const middlewares = require('../../src/middlewares')
 const redirectRoute = require('../../src/routes/redirect')
 const urlsRoute = require('../../src/routes/urls')
+const { indexes, client } = require('../../src/repository')
 const app = require('express')()
 middlewares.forEach(middleware => {
 	app.use(middleware)
@@ -9,9 +10,19 @@ middlewares.forEach(middleware => {
 app.use(redirectRoute)
 app.use(urlsRoute)
 const request = require('supertest')(app)
+const api = '/api/urls/'
 
 describe('App - E2E', () => {
-	it('', () => {
+	before(async () => {
+		expect(await indexes.create()).string('OK')
+	});
 
+	it('Create Short Url', async () => {
+		const res = await request.post(api).send({ url: 'test.com' })
+		expect(res.body.views).equal(0)
+	});
+
+	after(async () => {
+		expect(await client.FLUSHDB()).string('OK')
 	});
 });
