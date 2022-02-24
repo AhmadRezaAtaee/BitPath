f2569ab849000e6eb92732a506f63d7be25b8971
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-24 12:07:20 +0330

    feat: use middlewares

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 require('dotenv/config')
+const middlewares = require('./middlewares')
+const redirectRoute = require('./routes/redirect')
+const urlsRoute = require('./routes/urls')
 
 const app = require('express')()
+middlewares.forEach(middleware => {
+	app.use(middleware)
+})
 
 app.listen(process.env.SERVER_PORT || 3000)
